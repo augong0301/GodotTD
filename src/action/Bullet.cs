b7db7699430c9fff0b9c57cf3c6c538a63328473
翔宇 龚 <gongxy0301@163.com>
2025-04-13 23:05:51 +0800

    add function for generate bullet

--- src/action/Bullet.cs
+++ src/action/Bullet.cs
@@ -3,7 +3,10 @@ using System;
 
 public partial class Bullet : Node2D
 {
+	public int Damage { get; set; }
+
 	// Called when the node enters the scene tree for the first time.
+
 	public override void _Ready()
 	{
 	}
@@ -11,5 +14,10 @@ public partial class Bullet : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	internal void Init(Enemy enemy)
+	{
+
 	}
 }
--- src/action/Tower.cs
+++ src/action/Tower.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 public partial class Tower : Node2D
 {
@@ -8,6 +9,72 @@ public partial class Tower : Node2D
 	private List<Enemy> Enemies = new List<Enemy>();
 	private Sprite2D Turret;
 	private Node2D Fort;
+	private AudioStreamPlayer2D AudioStreamPlayer;
+	private Timer AttackTimer;
+	#region Bullet
+	[Export]
+	public int CoolDown { get; set; } = 2;
+
+	[Export]
+	public int BulletCount { get; set; } = 2;
+
+	[Export]
+	public int ReloadTime { get; set; } = 2;
+	public bool IsReloading { get; set; } = false;
+	private Timer ReloadTimer;
+
+	public int CurrentBulletCount { get; set; }
+
+	[Export]
+	public int Damage { get; set; }
+
+	public void Attack()
+	{
+		if (Enemies == null || Enemies.Count == 0) return;
+
+		// 如果子弹耗尽，开始重新装弹
+		if (CurrentBulletCount <= 0)
+		{
+			StartReload();
+			return;
+		}
+		GD.Print($"Attack on CurrentBulletCount = {CurrentBulletCount}.");
+		GenerateBullet(Enemies[0]);
+		CurrentBulletCount--;  // 每次攻击消耗一颗子弹
+
+		// 如果这是最后一颗子弹，开始重新装弹
+		if (CurrentBulletCount <= 0)
+		{
+			StartReload();
+		}
+	}
+
+	private void StartReload()
+	{
+		if (IsReloading) return;  // 已经在重新装弹
+
+		IsReloading = true;
+		GD.Print("开始重新装弹...");
+		ReloadTimer.Start(ReloadTime);
+	}
+
+	public void GenerateBullet(Enemy enemy)
+	{
+		var scene = ResourceLoader.Load<PackedScene>("res://src/action/Bullet.tscn");
+		DefaultBullet = scene.Instantiate<Bullet>() ?? throw new Exception("DefaultBullet cast failed");
+		if (DefaultBullet != null)
+		{
+			DefaultBullet.Damage = Damage;
+			AddChild(DefaultBullet);
+
+			DefaultBullet.Init(enemy);
+			CurrentBulletCount -= 1;
+		}
+	}
+
+	[Export]
+	public Bullet DefaultBullet;
+	#endregion
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -15,9 +82,36 @@ public partial class Tower : Node2D
 		Area = GetNode<Area2D>("Area2D");
 		Turret = GetNode<Sprite2D>("./Fort/Turret");
 		Fort = GetNode<Node2D>("./Fort");
+		AudioStreamPlayer = GetNode<AudioStreamPlayer2D>("AudioStreamPlayer2D");
+
+		AttackTimer = new Timer();
+		AddChild(AttackTimer);
+		AttackTimer.Timeout += OnAttackTimerTimeout;
+		AttackTimer.Start(CoolDown);
+
+		ReloadTimer = new Timer();
+		AddChild(ReloadTimer);
+		ReloadTimer.Timeout += OnReloadComplete;
+
 		Area.AreaEntered += _area_AreaEntered;
 		Area.AreaExited += _area_AreaExited;
 	}
+
+	private void OnReloadComplete()
+	{
+		CurrentBulletCount = BulletCount;
+		IsReloading = false;
+		GD.Print("重新装弹完成！");
+	}
+
+	private void OnAttackTimerTimeout()
+	{
+		if (Enemies.Count > 0 && IsInstanceValid(Enemies[0]) && !IsReloading)
+		{
+			Attack();
+		}
+	}
+
 	private void _area_AreaEntered(Area2D area)
 	{
 		Enemies.Add(area.Owner as Enemy);
@@ -37,10 +131,7 @@ public partial class Tower : Node2D
 			if (IsInstanceValid(Enemies[0]))
 			{
 				Fort.LookAt(Enemies[0].GlobalPosition);
-			}
-			else
-			{
-				Enemies.RemoveAt(0);
+				Attack();
 			}
 		}
 	}
